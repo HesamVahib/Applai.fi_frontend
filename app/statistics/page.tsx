ad0cc3777e0dcec6b304125ca2d8cddc0b93785e
Hesam V. <hesam.vahib@gmail.com>
2025-08-26 21:23:50 +0300

    adding the different path in env

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,8 +1,8 @@
 export default function StatisticsPage() {
   return (
-    <div>
-      <h1>Statistics</h1>
-      <p>This is the statistics page.</p>
+    <div className="p-8 -end">
+      <h1 className="text-3xl font-bold">Statistics</h1>
+      <p className="mt-4">This is the statistics of Finland Job Market page.</p>
     </div>
   );
 }

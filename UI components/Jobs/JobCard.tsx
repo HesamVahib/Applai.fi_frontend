d558a4da7d637674ec21850ceb614b563bbce634
Hesam V. <hesam.vahib@gmail.com>
2025-09-06 13:02:06 +0300

    changed job cards to article to be understood by google search

--- UI components/Jobs/JobCard.tsx
+++ UI components/Jobs/JobCard.tsx
@@ -4,21 +4,23 @@ import { Job } from "@/lib/types";
 export default function JobCard({ job } : { job: Job }) {
 
   return (
-    <div className="flex justify-between items-center border-b-2 pb-3 border-gray-300 py-4">
-    <div key={job.id} className="">
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href={job.link}
-        className="text-blue-500 underline">
-        <h3 className="text-lg font-semibold">{job.title}</h3>
-      </a>
-      <p className="text-sm text-gray-500 mt-1">
-        {job.company} - {job.city} - {job.date}
-      </p>
-    </div>
-    <FavoriteIcon jobId={job.id} />
-    </div>
+    <article>
+        <div className="flex justify-between items-center border-b-2 pb-3 border-gray-300 py-4">
+          <div key={job.id} className="">
+            <a
+              target="_blank"
+          rel="noopener noreferrer"
+          href={job.link}
+          className="text-blue-500 underline">
+          <h3 className="text-lg font-semibold">{job.title}</h3>
+        </a>
+        <p className="text-sm text-gray-500 mt-1">
+          {job.company} - {job.city} - {job.date}
+        </p>
+      </div>
+      <FavoriteIcon jobId={job.id} />
+      </div>
+    </article>
 
   );
 }
